536ce2e1d8d7440de73661c8d788818b3b789649
hgcummings <harry@xlr3.com>
2014-08-21 23:54:41 +0100

    Add notice for non-JS/non-MIDI users

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 require(['menu', 'models/midi', 'view', 'model'], function(menu, midi, view, model) {
-    navigator.requestMIDIAccess().then(function(midiAccess) {
-        var midiModel = midi.init(midiAccess);
-        menu.init(document.getElementById('menu'), midiModel);
-        view.init(document.getElementById('game'), model.init());
-    });
+    if (navigator && navigator.requestMIDIAccess()) {
+        document.getElementById('warning').remove();
+        navigator.requestMIDIAccess().then(function(midiAccess) {
+            var midiModel = midi.init(midiAccess);
+            menu.init(document.getElementById('menu'), midiModel);
+            view.init(document.getElementById('game'), model.init());
+        });
+    }
 });
