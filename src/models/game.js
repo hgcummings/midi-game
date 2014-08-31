bce82cbe21ed83ae76eff7db09c9b97ea7bc954b
hgcummings <harry@xlr3.com>
2014-08-31 21:20:39 +0100

    Factor paddle out of game model

--- src/models/game.js
+++ src/models/game.js
@@ -1,28 +1,16 @@
-define(['models/level', 'data/constants'], function(loader, constants) {
+define(['models/level', 'models/paddle', 'data/constants'], function(loader, paddle, constants) {
     return {
         init: function(level, input) {
             var self = {};
             var prevTime = 0;
 
-            var leftBound = constants.PADDLE.MARGIN.X + constants.PADDLE.SIZE.X / 2;
-            var rightBound = constants.WIDTH - constants.PADDLE.MARGIN.X - constants.PADDLE.SIZE.X / 2;
-
             self.update = function(gameTime) {
-                self.paddle.x += input.getDirection() * self.paddle.speed * (gameTime - prevTime);
-
-                if (self.paddle.x < leftBound) {
-                    self.paddle.x = leftBound;
-                } else if (self.paddle.x > rightBound) {
-                    self.paddle.x = rightBound;
-                }
-
+                var delta = gameTime - prevTime;
+                self.paddle.update(delta, input.getDirection());
                 prevTime = gameTime;
             };
             self.blocks = loader.load(level);
-            self.paddle = {
-                x: constants.WIDTH / 2,
-                speed: constants.WIDTH / 4000
-            }
+            self.paddle = paddle.init();
 
             return self;
         }
---
+++ src/models/paddle.js
@@ -0,0 +1,24 @@
+define(['data/constants'], function(constants) {
+    return {
+        init: function() {
+            var self = {};
+            var speed = constants.WIDTH / 4000;
+            var leftBound = constants.PADDLE.MARGIN.X + constants.PADDLE.SIZE.X / 2;
+            var rightBound = constants.WIDTH - constants.PADDLE.MARGIN.X - constants.PADDLE.SIZE.X / 2;
+
+            self.update = function(delta, direction) {
+                self.x += direction * speed * delta;
+
+                if (self.x < leftBound) {
+                    self.x = leftBound;
+                } else if (self.x > rightBound) {
+                    self.x = rightBound;
+                }
+            };
+
+            self.x = constants.WIDTH / 2;
+
+            return self;
+        }
+    }
+});
